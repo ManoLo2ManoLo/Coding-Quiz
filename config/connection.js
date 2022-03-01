3a18c5d3042fb200b0902f560a4c34c63efd8b03
ManoLo2ManoLo <manuel_canas@aol.com>
2022-03-01 02:29:16 -0500

    create handlebars and their routes

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-        sequelize = new Sequelize(process.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
@@ -13,5 +13,5 @@ if (process.env.JAWSDB_URL) {
         port: 3306
     });
 }
-  
-  module.exports = sequelize;
+
+module.exports = sequelize;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,5 +4,15 @@ const { User, Score } = require('../Models');
 const { withAuth, withNoAuth } = require('../utils/auth.js');
 
 router.get('/', (req, res) => {
-    
+    res.render('home')
 });
+
+router.get('/login', (req, res) => {
+    res.render('login')
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup')
+});
+
+module.exports = router;
